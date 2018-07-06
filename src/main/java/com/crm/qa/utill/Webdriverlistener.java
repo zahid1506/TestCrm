b58d49743a082b350c9323e6fddc635525800568
zahid khan <zahidkhan.1008@gmail.com>
2018-07-06 15:18:42 +0530

    final utill

--- src/main/java/com/crm/qa/utill/Webdriverlistener.java
+++ src/main/java/com/crm/qa/utill/Webdriverlistener.java
@@ -10,57 +10,110 @@ import com.crm.qa.base.BaseClass;
 
 public class Webdriverlistener extends BaseClass implements WebDriverEventListener {
 
-	public void afterAlertAccept(WebDriver arg0) {
-		// TODO Auto-generated method stub
-		
+	public void beforeNavigateTo(String url, WebDriver driver) {
+		System.out.println("Before navigating to: '" + url + "'");
 	}
 
-	public void afterAlertDismiss(WebDriver arg0) {
-		// TODO Auto-generated method stub
-		
+	public void afterNavigateTo(String url, WebDriver driver) {
+		System.out.println("Navigated to:'" + url + "'");
 	}
 
-	public void afterChangeValueOf(WebElement arg0, WebDriver arg1, CharSequence[] arg2) {
-		// TODO Auto-generated method stub
-		
+	public void beforeChangeValueOf(WebElement element, WebDriver driver) {
+		System.out.println("Value of the:" + element.toString() + " before any changes made");
 	}
 
-	public void afterClickOn(WebElement arg0, WebDriver arg1) {
-		// TODO Auto-generated method stub
-		
+	public void afterChangeValueOf(WebElement element, WebDriver driver) {
+		System.out.println("Element value changed to: " + element.toString());
 	}
 
-	public void afterFindBy(By arg0, WebElement arg1, WebDriver arg2) {
-		// TODO Auto-generated method stub
+	public void beforeClickOn(WebElement element, WebDriver driver) {
+		System.out.println("Trying to click on: " + element.toString());
+	}
+
+	public void afterClickOn(WebElement element, WebDriver driver) {
+		System.out.println("Clicked on: " + element.toString());
+	}
+
+	public void beforeNavigateBack(WebDriver driver) {
+		System.out.println("Navigating back to previous page");
+	}
+
+	public void afterNavigateBack(WebDriver driver) {
+		System.out.println("Navigated back to previous page");
+	}
+
+	public void beforeNavigateForward(WebDriver driver) {
+		System.out.println("Navigating forward to next page");
+	}
+
+	public void afterNavigateForward(WebDriver driver) {
+		System.out.println("Navigated forward to next page");
+	}
+
+	public void onException(Throwable error, WebDriver driver) {
+		System.out.println("Exception occured: " + error);
 		
+		}
+
+
+	public void beforeFindBy(By by, WebElement element, WebDriver driver) {
+		System.out.println("Trying to find Element By : " + by.toString());
+	}
+
+	public void afterFindBy(By by, WebElement element, WebDriver driver) {
+		System.out.println("Found Element By : " + by.toString());
+	}
+
+	/*
+	 * non overridden methods of WebListener class
+	 */
+	public void beforeScript(String script, WebDriver driver) {
+	}
+
+	public void afterScript(String script, WebDriver driver) {
+	}
+
+	public void beforeAlertAccept(WebDriver driver) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void afterAlertAccept(WebDriver driver) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void afterAlertDismiss(WebDriver driver) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void beforeAlertDismiss(WebDriver driver) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void beforeNavigateRefresh(WebDriver driver) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void afterNavigateRefresh(WebDriver driver) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void beforeChangeValueOf(WebElement element, WebDriver driver, CharSequence[] keysToSend) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void afterChangeValueOf(WebElement element, WebDriver driver, CharSequence[] keysToSend) {
+		// TODO Auto-generated method stub
+
 	}
 
 	public <X> void afterGetScreenshotAs(OutputType<X> arg0, X arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void afterNavigateBack(WebDriver arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void afterNavigateForward(WebDriver arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void afterNavigateRefresh(WebDriver arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void afterNavigateTo(String arg0, WebDriver arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void afterScript(String arg0, WebDriver arg1) {
 		// TODO Auto-generated method stub
 		
 	}
@@ -70,57 +123,7 @@ public class Webdriverlistener extends BaseClass implements WebDriverEventListen
 		
 	}
 
-	public void beforeAlertAccept(WebDriver arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void beforeAlertDismiss(WebDriver arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void beforeChangeValueOf(WebElement arg0, WebDriver arg1, CharSequence[] arg2) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void beforeClickOn(WebElement arg0, WebDriver arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void beforeFindBy(By arg0, WebElement arg1, WebDriver arg2) {
-		// TODO Auto-generated method stub
-		
-	}
-
 	public <X> void beforeGetScreenshotAs(OutputType<X> arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void beforeNavigateBack(WebDriver arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void beforeNavigateForward(WebDriver arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void beforeNavigateRefresh(WebDriver arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void beforeNavigateTo(String arg0, WebDriver arg1) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void beforeScript(String arg0, WebDriver arg1) {
 		// TODO Auto-generated method stub
 		
 	}
@@ -130,10 +133,6 @@ public class Webdriverlistener extends BaseClass implements WebDriverEventListen
 		
 	}
 
-	public void onException(Throwable arg0, WebDriver arg1) {
-		// TODO Auto-generated method stub
-		
-	}
 
 	
 }
